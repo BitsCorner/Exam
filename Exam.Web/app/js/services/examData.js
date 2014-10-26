8204db71c24ba64efb7f14a9acaaf9a2e06b0beb
aramkoukia <aramkoukia@gmail.com>
2014-10-26 12:32:46 -0700

    Fixed some layout issues

--- Exam.Web/app/js/services/examData.js
+++ Exam.Web/app/js/services/examData.js
@@ -1,9 +1,10 @@
 'use strict';
 
+var baseUrl = "http://localhost/ExamSvc/api/";
 examApp.factory('examData', function ($http, $log) {
     return {
         getCertificates: function (successcb) {
-            $http({ method: "GET", url: "http://localhost/ExamSvc/api/Certificate" })
+            $http({ method: "GET", url: baseUrl + "Certificate" })
             .success(function (data, status, header, config) {
                 successcb(data);
             })
