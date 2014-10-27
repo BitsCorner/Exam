5111dc4d648a282a94dccc48badd0c84cf426a37
aramkoukia <aramkoukia@gmail.com>
2014-10-27 16:47:30 -0700

    Added routing to web project

--- Exam.Web/app/js/app.js
+++ Exam.Web/app/js/app.js
@@ -1,3 +1,17 @@
 'use strict';
 
-var examApp = angular.module('examApp', ['ngResource']);
+var examApp = angular.module('examApp', ['ngResource', 'ui.bootstrap', 'ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider.when('/Certificates',
+            {
+                templateUrl: 'templates/Certificates.html',
+                controller: 'CertificateController'
+            });
+        $routeProvider.when('/DumpQuestion/:certificateId',
+            {
+                templateUrl: 'templates/DumpQuestion.html',
+                controller: 'QuestionController'
+            });
+        $routeProvider.otherwise({ redirectTo: '/Certificates' });
+
+    });
